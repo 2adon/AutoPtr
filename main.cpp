1294f0af679237a7d7cd0f8c747b7c50cc6bcea1
Kamsi Ozo <86496597+2adon@users.noreply.github.com>
2021-11-08 20:02:14 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+int main(int argc, char* argv, char* envp)
+{
+    int x = 10;
+    int y = 12;
+    AutoPtr<int> ap = &x;
+    AutoPtr<int> bp = &y;
+    ap = bp;
+
+    std::cout << *ap;
+
+    return 0;
+}
